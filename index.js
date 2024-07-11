963bc85e61f28aab98b850d003a139344f453f9b
Matt Castoro <matthewjcastoro@gmail.com>
2024-07-11 16:13:02 -0600

    Write operate function
    Write function that takes in an operator and two numbers that calls
    the correct operation function based on the operator provided.

--- index.js
+++ index.js
@@ -13,3 +13,23 @@ function calcProduct(a, b) {
 function calcDivision(a, b) {
     return a / b;
 }
+
+let num1;
+let operator;
+let num2;
+
+function operate(operator, num1, num2) {
+    switch (operator) {
+        case "+" :
+            return calcSum(num1, num2);
+            break;
+        case "-" :
+            return calcDifference(num1, num2);
+            break;
+        case "*" :
+            return calcProduct(num1, num2);
+            break;
+        case "/" :
+            return calcDivision(num1, num2);
+    }
+}
