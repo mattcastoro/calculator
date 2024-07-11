0da20aabc7b161d97aeacb17cd55f889d890567d
Matt Castoro <matthewjcastoro@gmail.com>
2024-07-11 15:42:05 -0600

    Write basic math functions
    Write functions for add, subtract, multiply, and divide.

---
+++ index.js
@@ -0,0 +1,15 @@
+function calcSum(a, b) {
+    return a + b;
+}
+
+function calcDifference(a, b) {
+    return a - b;
+}
+
+function calcProduct(a, b) {
+    return a * b;
+}
+
+function calcDivision(a, b) {
+    return a / b;
+}
