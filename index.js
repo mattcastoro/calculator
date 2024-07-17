d07e6ac218fb38e4c84c50d8365b49e8e5f780dc
Matt Castoro <matthewjcastoro@gmail.com>
2024-07-17 15:23:36 -0600

    Rework clr function and basic calculator operation
    Rewrite clr function to actually clear out all values so the user can
    start fresh.  Update secondValue function to allow multiple operations
    after the user clicks the equals sign.

--- index.js
+++ index.js
@@ -15,27 +15,38 @@ function calcDivision(a, b) {
     return a / b;
 }
 
-let num1;
+// declared global variables to store values for math operations //
+let firstValue;
 let operator;
-let num2;
+let secondValue;
+// let firstValueCleaned;
+// let secondValueCleaned;
 
-function operate(operator, num1, num2) {
+// completes the math operations //
+function operate(operator, firstValue, secondValue) {
+    let firstValueCleaned = Number(firstValue);
+    let secondValueCleaned = Number(secondValue);
     switch (operator) {
         case "+" :
-            return calcSum(num1, num2);
+            document.getElementById("inputDisplay").value = calcSum(firstValueCleaned, secondValueCleaned);
+            console.log(`total(${calcSum(firstValueCleaned, secondValueCleaned)})`)
             break;
         case "-" :
-            return calcDifference(num1, num2);
+            document.getElementById("inputDisplay").value = calcDifference(firstValueCleaned, secondValueCleaned);
+            console.log(`total(${calcDifference(firstValueCleaned, secondValueCleaned)})`)
             break;
         case "*" :
-            return calcProduct(num1, num2);
+            document.getElementById("inputDisplay").value = calcProduct(firstValueCleaned, secondValueCleaned);
+            console.log(`total(${calcProduct(firstValueCleaned, secondValueCleaned)})`)
             break;
         case "/" :
-            return calcDivision(num1, num2);
+            document.getElementById("inputDisplay").value = calcDivision(firstValueCleaned, secondValueCleaned);
+            console.log(`total(${calcDivision(firstValueCleaned, secondValueCleaned)})`)
+            break;
     }
 }
 
-// displays value based on the button a user clicked //
+// displays value based on the number keys a user clicks //
 document.addEventListener("DOMContentLoaded", function() {
     let textBox = document.querySelector("#inputDisplay");
     let myButtons = document.querySelectorAll(".btn");
@@ -47,7 +58,37 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 });
 
-// clears input field
+// stores operator and current display values upon given operator being clicked by user //
+const myBtnOp = document.querySelectorAll(".btnOp");
+myBtnOp.forEach((btn) => {
+    btn.addEventListener("click", () => {
+       firstValue = document.getElementById("inputDisplay").value;
+       console.log(`firstValue(${firstValue})`)
+       operator = btn.value;
+       console.log(`operator(${operator})`)
+       getsecondValue();
+    });
+});
+
+// clears input display, stores secondValue, then calls operate function //
+function getsecondValue() {
+    clearInputDisplay();
+    const myBtnEquals = document.querySelector("#btnEquals");
+    myBtnEquals.onclick = function() {
+        console.log("Equal was clicked");
+        secondValue = document.getElementById("inputDisplay").value;
+        console.log(`secondValue(${secondValue})`)
+        console.log(`operator(${operator}) firstValue(${firstValue}) secondValue(${secondValue})`)
+        operate(operator, firstValue, secondValue);
+    };
+}
+
+// clears input display //
 function clearInputDisplay() {
     document.getElementById("inputDisplay").value="";
+}
+
+// reloads page to clear global variables and the input display //
+function resetPage() {
+    window.location.reload();
 }
