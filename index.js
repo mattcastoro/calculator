68fe3490cf96de23d576d2ded1d665fd96aad79d
Matt Castoro <matthewjcastoro@gmail.com>
2024-07-18 11:44:02 -0600

    Clean up
    Update the functions related to handling multiple operators for a given
    operation.

--- index.js
+++ index.js
@@ -2,15 +2,12 @@
 function calcSum(a, b) {
     return a + b;
 }
-
 function calcDifference(a, b) {
     return a - b;
 }
-
 function calcProduct(a, b) {
     return a * b;
 }
-
 function calcDivision(a, b) {
     return a / b;
 }
@@ -19,29 +16,25 @@ function calcDivision(a, b) {
 let firstValue;
 let operator;
 let secondValue;
-// let firstValueCleaned;
-// let secondValueCleaned;
 
 // completes the math operations //
 function operate(operator, firstValue, secondValue) {
-    let firstValueCleaned = Number(firstValue);
-    let secondValueCleaned = Number(secondValue);
     switch (operator) {
         case "+" :
-            document.getElementById("inputDisplay").value = calcSum(firstValueCleaned, secondValueCleaned);
-            console.log(`total(${calcSum(firstValueCleaned, secondValueCleaned)})`)
+            document.getElementById("inputDisplay").value = 
+            calcSum(Number(firstValue), Number(secondValue));
             break;
         case "-" :
-            document.getElementById("inputDisplay").value = calcDifference(firstValueCleaned, secondValueCleaned);
-            console.log(`total(${calcDifference(firstValueCleaned, secondValueCleaned)})`)
+            document.getElementById("inputDisplay").value = 
+            calcDifference(Number(firstValue), Number(secondValue));
             break;
         case "*" :
-            document.getElementById("inputDisplay").value = calcProduct(firstValueCleaned, secondValueCleaned);
-            console.log(`total(${calcProduct(firstValueCleaned, secondValueCleaned)})`)
+            document.getElementById("inputDisplay").value = 
+            calcProduct(Number(firstValue), Number(secondValue));
             break;
         case "/" :
-            document.getElementById("inputDisplay").value = calcDivision(firstValueCleaned, secondValueCleaned);
-            console.log(`total(${calcDivision(firstValueCleaned, secondValueCleaned)})`)
+            document.getElementById("inputDisplay").value = 
+            calcDivision(Number(firstValue), Number(secondValue));
             break;
     }
 }
@@ -58,27 +51,20 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 });
 
-// stores operator and current display values upon given operator being clicked by user //
 const myBtnOp = document.querySelectorAll(".btnOp");
 myBtnOp.forEach((btn) => {
     btn.addEventListener("click", () => {
-       firstValue = document.getElementById("inputDisplay").value;
-       console.log(`firstValue(${firstValue})`)
-       operator = btn.value;
-       console.log(`operator(${operator})`)
-       getsecondValue();
+        firstValue = document.getElementById("inputDisplay").value;
+        operator = btn.value;
+        getSecondValue();
     });
 });
 
-// clears input display, stores secondValue, then calls operate function //
-function getsecondValue() {
+function getSecondValue() {
     clearInputDisplay();
     const myBtnEquals = document.querySelector("#btnEquals");
     myBtnEquals.onclick = function() {
-        console.log("Equal was clicked");
         secondValue = document.getElementById("inputDisplay").value;
-        console.log(`secondValue(${secondValue})`)
-        console.log(`operator(${operator}) firstValue(${firstValue}) secondValue(${secondValue})`)
         operate(operator, firstValue, secondValue);
     };
 }
