f104a6d55220dec55d81700cc4c45523ddce2e00
Matt Castoro <matthewjcastoro@gmail.com>
2024-07-19 15:24:37 -0600

    Basic operations are working per design
    Math operations and executions are working after some code rework

--- index.js
+++ index.js
@@ -1,80 +1,135 @@
-// basic math functions for adding, subtracting, multiplying, and division //
+// GLOBAL VARIABLES to store values for OPERATION HANDLER //
+let firstValue = "";
+let secondValue = "";
+let operator;
+let previousOperator;
+let result;
+let operatorList = ["+", "-", "*", "/"];
+let fullOperatorList = ["+", "-", "*", "/", "="];
+let lastButtonClicked;
+
+// OPERATION HANDLER for adding, subtracting, multiplying, and division //
 function calcSum(a, b) {
-    return a + b;
+    result = a + b;
+    return parseFloat(result.toFixed(2))
 }
 function calcDifference(a, b) {
-    return a - b;
+    result = a - b;
+    return parseFloat(result.toFixed(2))
 }
 function calcProduct(a, b) {
-    return a * b;
+    result = a * b;
+    return parseFloat(result.toFixed(2))
 }
 function calcDivision(a, b) {
-    return a / b;
+    result = a / b;
+    return parseFloat(result.toFixed(2))
 }
 
-// declared global variables to store values for math operations //
-let firstValue;
-let operator;
-let secondValue;
-
-// completes the math operations //
+// EXPRESSION HANDLER to complete math calculation //
 function operate(operator, firstValue, secondValue) {
     switch (operator) {
         case "+" :
-            document.getElementById("inputDisplay").value = 
-            calcSum(Number(firstValue), Number(secondValue));
+            result = calcSum(Number(firstValue), Number(secondValue));
+            document.getElementById("inputDisplay").value = result;
             break;
         case "-" :
-            document.getElementById("inputDisplay").value = 
-            calcDifference(Number(firstValue), Number(secondValue));
+            result = calcDifference(Number(firstValue), Number(secondValue));
+            document.getElementById("inputDisplay").value = result;
             break;
         case "*" :
-            document.getElementById("inputDisplay").value = 
-            calcProduct(Number(firstValue), Number(secondValue));
+            result = calcProduct(Number(firstValue), Number(secondValue));
+            document.getElementById("inputDisplay").value = result;
             break;
         case "/" :
-            document.getElementById("inputDisplay").value = 
-            calcDivision(Number(firstValue), Number(secondValue));
+            result = calcDivision(Number(firstValue), Number(secondValue));
+            document.getElementById("inputDisplay").value = result;
             break;
     }
 }
 
-// displays value based on the number keys a user clicks //
+// DISPLAY HANDLER to display number values upon user clicks //
 document.addEventListener("DOMContentLoaded", function() {
     let textBox = document.querySelector("#inputDisplay");
     let myButtons = document.querySelectorAll(".btn");
 
     myButtons.forEach(function(btn) {
         btn.addEventListener("click", function(e) {
-            textBox.value += this.value;
+            if(fullOperatorList.includes(lastButtonClicked)) {
+                clearInputDisplay();
+                textBox.value += this.value;
+            } else {
+                textBox.value += this.value;
+            }
         });
     });
 });
 
-const myBtnOp = document.querySelectorAll(".btnOp");
-myBtnOp.forEach((btn) => {
-    btn.addEventListener("click", () => {
-        firstValue = document.getElementById("inputDisplay").value;
-        operator = btn.value;
-        getSecondValue();
+// LAST BUTTON LOG to store the value of the last clicked button //
+document.addEventListener("DOMContentLoaded", function() {
+    let myButtons = document.querySelectorAll("button");
+
+    myButtons.forEach(function(btn) {
+        btn.addEventListener("click", function(e) {
+            lastButtonClicked = e.target.value;
+        });
     });
 });
 
-function getSecondValue() {
-    clearInputDisplay();
-    const myBtnEquals = document.querySelector("#btnEquals");
-    myBtnEquals.onclick = function() {
-        secondValue = document.getElementById("inputDisplay").value;
-        operate(operator, firstValue, secondValue);
-    };
+// EVENT HANDLER to store operator clicks //
+const myBtn = document.querySelectorAll(".btnOp");
+let tempOpList = [];
+myBtn.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+        tempOpList.unshift(e.target.value);
+        operator = tempOpList[0];
+        previousOperator = tempOpList[1];
+        execute();
+    })
+});
+
+// EXECUTION HANDLER to perform logical calculation setups //
+function execute() {
+    if (operatorList.includes(operator)) {
+        if (document.getElementById("inputDisplay").value == "") {
+            alert("Please enter a number before selecting a math operator");
+            resetPage();
+        } else if (firstValue == "") {
+            firstValue = document.getElementById("inputDisplay").value;
+        } else if (firstValue != "" && secondValue == "") {
+            secondValue = document.getElementById("inputDisplay").value;
+            operate(previousOperator, firstValue, secondValue);
+            console.log(firstValue, previousOperator, secondValue, "equals", result);
+        } else if (firstValue != "" && secondValue != "") {
+            firstValue = result;
+            secondValue = document.getElementById("inputDisplay").value;
+            operate(previousOperator, firstValue, secondValue);
+            console.log(firstValue, previousOperator, secondValue, "equals", result);
+        }
+    } else if (operator == "=") {
+        if(document.getElementById("inputDisplay").value == "") {
+            alert("Please select a number, math operator, and another number before selecting equals");
+            resetPage();
+        } else if (firstValue != "" && secondValue != "") {
+            firstValue = result;
+            secondValue = document.getElementById("inputDisplay").value;
+            operate(previousOperator, firstValue, secondValue);
+            console.log(firstValue, previousOperator, secondValue, "equals", result);
+        } else {
+            secondValue = document.getElementById("inputDisplay").value;
+            operate(previousOperator, firstValue, secondValue);
+            console.log(firstValue, previousOperator, secondValue, "equals", result);
+        }
+
+    }
 }
 
-// clears input display //
+// DISPLAY CLEARING HANDLER to clear input display //
 function clearInputDisplay() {
     document.getElementById("inputDisplay").value="";
 }
 
-// reloads page to clear global variables and the input display //
+// RESET HANDLER to clear global variables and the input display //
 function resetPage() {
     window.location.reload();
 }
